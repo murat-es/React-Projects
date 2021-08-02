d343dfd49215c8a325d68f0aec37439213d39406
murateist <murateist@users.noreply.github.com>
2021-08-02 09:57:06 +0300

    sticky

--- spendmoney/src/components/Header.js
+++ spendmoney/src/components/Header.js
@@ -5,7 +5,7 @@ import'./header.css'
 export default function Header({money,total}) {
     
     return (
-        <div >
+        <div className="head" >
             {money-total>0 ? <div className="header1">You have ₺{moneyFormat(money-total)}</div>    :
              <div className="header2">You spent all the money</div> }
            
